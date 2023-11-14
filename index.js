58bdd4e041396bd042da003c071a59c6f4a4d4a5
Carolina Naar <132184914+naardiana@users.noreply.github.com>
2023-11-13 22:13:13 -0800

    Update index.js
    Added // Find all the buttons on the page
    const buttons = document.querySelectorAll("button");
    console.log(buttons);

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log(document);
 const heading = document.querySelector("h1");
 console.log(heading);
+// Find all the buttons on the page
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
